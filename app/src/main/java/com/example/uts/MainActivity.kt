4ccb3d52a39cca5763e2ab907e033decaa4eb868
rockyRS2 <rockyramdhansetiawan@gmail.com>
2024-07-31 14:20:01 +0700

    update uas

--- app/src/main/java/com/example/uts/MainActivity.kt
+++ app/src/main/java/com/example/uts/MainActivity.kt
@@ -2,57 +2,152 @@ package com.example.uts
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val RC_SIGN_IN = 9001
+        private const val TAG = "LoginActivity"
+    }
+
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+    private lateinit var mAuth : FirebaseAuth
+    private lateinit var progressBar: ProgressBar
+    private lateinit var googleSignInButton : Button
+    lateinit var register: TextView
+    lateinit var loginButton: Button
+    lateinit var emailInput : EditText
+    lateinit var passwordInput : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val etEmail: EditText = findViewById(R.id.etEmail)
-        val etPassword: EditText = findViewById(R.id.etPassword)
-        val btnLogin: Button = findViewById(R.id.btnLogin)
-        val tvRegister: TextView = findViewById(R.id.tvRegister)
 
-        btnLogin.setOnClickListener {
-            val email = etEmail.text.toString().trim()
-            val password = etPassword.text.toString().trim()
+        register = findViewById(R.id.tvRegister)
+        loginButton=findViewById(R.id.btnLogin)
+        emailInput = findViewById(R.id.email)
+        passwordInput = findViewById(R.id.password)
 
-            val sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE)
-            val registeredEmail = sharedPreferences.getString("email", null)
-            val registeredPassword = sharedPreferences.getString("password", null)
+        loginButton.setOnClickListener {
+            val email = emailInput.text.toString().trim()
+            val password = passwordInput.text.toString().trim()
 
-            if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
-                Log.d("LoginActivity", "Some fields are empty")
-            } else if (email == registeredEmail && password == registeredPassword) {
-                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
-                Log.d("LoginActivity", "Login successful")
+            if (TextUtils.isEmpty(email)) {
+                emailInput.error = "Email is required"
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)) {
+                passwordInput.error = "Password is required"
+                return@setOnClickListener
+            }
 
-                // Start ListActivity
-                val intent = Intent(this, ListActivity::class.java)
-                startActivity(intent)
-                finish()
-            } else {
-                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
-                Log.d("LoginActivity", "Invalid credentials")
+            emailPasswordLogin(email, password)
+        }
+
+
+        googleSignInButton =findViewById(R.id.googleSignInButton)
+        progressBar = findViewById(R.id.progressBar)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
+        mAuth = FirebaseAuth.getInstance()
+
+        googleSignInButton.setOnClickListener {
+            signIn()
+        }
+
+        register.setOnClickListener {
+            val intent = Intent(this@MainActivity,RegisActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun emailPasswordLogin(email: String, password: String) {
+        progressBar.visibility = View.VISIBLE
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                progressBar.visibility = View.GONE
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = mAuth.currentUser
+                    Toast.makeText(this, "Authentication Successful.", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, Dashboard::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(this, "Authentication Failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun fireBaseAuthWithGoogle(acct : GoogleSignInAccount){
+        Log.d(TAG,"fireBaseAuthWithGoogle:"+ acct.id)
+        val credential: AuthCredential = GoogleAuthProvider.getCredential(acct.idToken,null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener (this){task->
+                if(task.isSuccessful){
+                    Log.d(TAG,"signInWithCredential successful.")
+                    val user : FirebaseUser? = mAuth.currentUser
+                    Toast.makeText(this,"Authentication Successful.",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,Dashboard::class.java)
+                    startActivity(intent)
+                    finish()
+                }else{
+                    Log.w(TAG,"signInWithCredential failed.")
+                    Toast.makeText(this,"Authentication Failed",Toast.LENGTH_SHORT).show()
+                }
+                progressBar.visibility = View.GONE
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                fireBaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "Google Sign In failed", e)
+                progressBar.visibility = View.GONE
+                Toast.makeText(this, "Google sign in failed : ${e.message}", Toast.LENGTH_LONG)
+                    .show()
             }
         }
-
-        tvRegister.setOnClickListener {
-            val intent = Intent(this, RegisActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-
     }
+
+    private fun signIn(){
+        progressBar.visibility = View.VISIBLE
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+
+
 }
--- app/src/main/java/com/example/uts/RegisActivity.kt
+++ app/src/main/java/com/example/uts/RegisActivity.kt
@@ -2,64 +2,114 @@ package com.example.uts
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RegisActivity : AppCompatActivity() {
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var nameEditText: EditText
+    private lateinit var registerButton: Button
+    private lateinit var progressBar: ProgressBar
+    lateinit var login: TextView
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_regis)
 
-        val etUsername: EditText = findViewById(R.id.etUsername)
-        val etEmail: EditText = findViewById(R.id.etEmail)
-        val etPassword: EditText = findViewById(R.id.etPassword)
-        val etConfirmPassword: EditText = findViewById(R.id.etConfirmPassword)
-        val btnRegister: Button = findViewById(R.id.btnRegister)
-        val tvLogin: TextView = findViewById(R.id.tvLogin)
+        // Inisialisasi Firebase
+        FirebaseApp.initializeApp(this)
+        mAuth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
 
-        btnRegister.setOnClickListener {
-            val username = etUsername.text.toString().trim()
-            val email = etEmail.text.toString().trim()
-            val password = etPassword.text.toString().trim()
-            val confirmPassword = etConfirmPassword.text.toString().trim()
-
-            if (username.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
-                Log.d("RegisActivity", "Some fields are empty")
-            } else if (password != confirmPassword) {
-                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
-                Log.d("RegisActivity", "Passwords do not match")
-            } else {
-                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
-                Log.d("RegisActivity", "Registration successful")
+        emailEditText = findViewById(R.id.email)
+        passwordEditText = findViewById(R.id.password)
+        nameEditText = findViewById(R.id.name)
+        registerButton = findViewById(R.id.registerButton)
+        progressBar = findViewById(R.id.progressBar)
+        login = findViewById(R.id.tvLogin)
 
 
-                val sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE)
-                val editor = sharedPreferences.edit()
-                editor.putString("username", username)
-                editor.putString("email", email)
-                editor.putString("password", password)
-                editor.apply()
 
-                // Start MainActivity
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+        registerButton.setOnClickListener {
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+            val name = nameEditText.text.toString().trim()
+
+            if (TextUtils.isEmpty(email)) {
+                emailEditText.error = "Email is required"
+                return@setOnClickListener
             }
+            if (TextUtils.isEmpty(password)) {
+                passwordEditText.error = "Password is required"
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(name)) {
+                nameEditText.error = "Name is required"
+                return@setOnClickListener
+            }
+
+            progressBar.visibility = View.VISIBLE
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    progressBar.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        val userId = mAuth.currentUser?.uid
+                        val user = hashMapOf(
+                            "name" to name,
+                            "email" to email
+                        )
+
+                        userId?.let {
+                            db.collection("users").document(it)
+                                .set(user)
+                                .addOnSuccessListener {
+                                    Toast.makeText(
+                                        this, "Register complete",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    val intent = Intent(this, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                                .addOnFailureListener { e ->
+                                    Toast.makeText(
+                                        this, "Error: ${e.message}",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                        }
+                    } else {
+                        Toast.makeText(
+                            this, "Registration Failed: ${task.exception?.message}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        Log.v("error", task.exception?.message ?: "Unknown error")
+                    }
+                }
+        }
+        login.setOnClickListener {
+            val intent = Intent(this@RegisActivity,MainActivity::class.java)
+            startActivity(intent)
         }
 
-        tvLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+
     }
 }
